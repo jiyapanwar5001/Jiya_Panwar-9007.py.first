3395f0320d87ae18978df65679b18b307c3e1f1e
Jiya Panwar <JiyaPanwar326@yahoo.com>
2025-08-26 15:30:39 +0530

    Update 8009.practice.py

--- 8009.practice.py
+++ 8009.practice.py
@@ -1,10 +1,10 @@
-# Python code
+# Python code 5001
 age= int(input())
 if 12<age<20:
   print("teenager")
 elif 20<age<25:
   print("no longer a teenager")
 elif age>=25:
-  print("adult")
+  print("not adult particularly")
 else:
   print("get out!")
